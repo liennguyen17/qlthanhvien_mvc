6bf93c57d7532a388768e2365c17b78e80000a84
liennguyen17 <nguyenlienltw@gmail.com>
2022-08-18 17:19:39 +0700

    update

---
+++ View/thanhvien/add_user.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thêm thành viên - Quản lý thành viên</title>
+</head>
+<body>
+    <div class="content">
+    <div class="dangkythanhvien">
+        <h3>Thêm mới thành viên</h3>
+        <form action="" method="POST">
+            <table>
+                <tr>
+                    <td>Tên thành viên : </td>
+                    <td><input type="text" name="hote" placeholder="Tên thành viên"></td>
+                </tr>
+                <tr>
+                    <td>Năm sinh :</td>
+                    <td><input type="text" name="namsinh" placeholder="Năm sinh"></td>
+                </tr>
+                <tr>
+                    <td>Quê quán :</td>
+                    <td><input type="text" name="quequan" placeholder="Quê quán"></td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td><input type="submit" name="add_user" placeholder="Thêm"></td>
+                </tr>
+            </table>
+        </form>
+        <a href="" class="list">Danh sách</a>
+        </div>
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,6 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quan ly thanh vien</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+</body>
+</html>
 <?php
 include "Model/DBConfig.php";
 $db = new Database;
 $db->connect();
 
+    if(isset($_GET['controller'])){
+        $controller = $_GET['controller'];
+    }
+    else{
+        $controller = '';
+    }
+
+    switch($controller){
+        case 'thanhvien':{
+            require_once('Controller/thanhvien/index.php');
+        }
+    }
 ?>
